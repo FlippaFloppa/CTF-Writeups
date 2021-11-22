aef0dbfd332ec24ca01068dbf29defd4d73eae63
blessed <patrickdifazio@blessed.local>
2021-11-22 17:35:55 +0100

    added cryptohack

--- Cryptohack/General/XOR/multi_xor.py
+++ Cryptohack/General/XOR/multi_xor.py
@@ -1,13 +1,13 @@
 import pwn as pwn
 from binascii import unhexlify
 
-#trasformo in bytes
+#transform in bytes
 KEY1=unhexlify("a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313")
 KEY4=unhexlify("04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf")
 KEY2KEY1=unhexlify("37dcb292030faa90d07eec17e3b1c6d8daf94c35d4c9191a5e1e")
 KEY2KEY3=unhexlify("c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1")
 
-#xor proprietà associativa
+#xor associative
 KEY2=(pwn.xor(KEY1,KEY2KEY1))
 KEY3=(pwn.xor(KEY2,KEY2KEY3))
 FLAG=(pwn.xor(KEY4,KEY3,KEY2,KEY1))
