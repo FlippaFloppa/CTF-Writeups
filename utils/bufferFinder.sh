85b34a24b5fdc76e69da5b1063892b064d17f37a
Luca3700 <91330738+Luca3700@users.noreply.github.com>
2021-11-24 23:44:21 +0100

    Add files via upload
    velocizzato il processo

--- utils/bufferFinder.sh
+++ utils/bufferFinder.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-for i in {1..1000}
+if [ $# -ne 1 ]
+then
+	echo Usage: ./buff2.sh nomefile
+	exit
+fi
+
+for i in {1..4096}
 do
-    error=$(./bof $(python -c "print('A'*$i)") 2>&1 1>tmp.txt); exit=$?;
-    if [ $exit -ne 0 ]
-    then 
-        echo Va in overflow da $i; break;
-    fi
+	#per velocizzare faccio ogni 32
+	if [ `expr $i % 32` -eq 0 ]
+	then
+		echo provo con $i
+		error=$($1 $(python -c "print('A'*$i)") 2>&1 1>tmp.txt); exit=$?;
+		if [ $exit -ne 0 ]
+		then 
+			echo Va in overflow da $i
+			num=`expr $i - 32`
+			for j in $(seq $num $i)
+			do	
+				echo provo con $j
+				error=$($1 $(python -c "print('A'*$j)") 2>&1 1>tmp.txt); exit=$?;
+				if [ $exit -ne 0 ]
+				then
+					echo Va in overflow da $j; exit;
+				fi
+			done
+		fi
+	fi
 done
